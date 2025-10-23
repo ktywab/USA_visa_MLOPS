611fdd993b5b1a826521038b7aae3d58007d4d38
ktywab <kossitonyiabotsi@gmail.com>
2025-10-23 17:54:40 +0000

    premier commit pour architecture de projet

---
+++ template.py
@@ -0,0 +1,75 @@
+#----------- Création automatique de l'architecture du projet -----------#
+
+import os
+from pathlib import Path
+
+project_name = "USA_visa_MLOPS"
+
+list_of_files = [
+
+    f"{project_name}/__init__.py",
+
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",  
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
+
+    f"{project_name}/configuration/__init__.py",
+
+    f"{project_name}/constants/__init__.py",
+
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifact_entity.py",
+
+    f"{project_name}/exception/__init__.py",
+
+    f"{project_name}/logger/__init__.py",
+
+    f"{project_name}/pipline/__init__.py",
+    f"{project_name}/pipline/training_pipeline.py",
+    f"{project_name}/pipline/prediction_pipeline.py",
+
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/main_utils.py",
+
+    "app.py",
+
+    "requirements.txt",
+
+    "Dockerfile",
+
+    ".dockerignore",
+
+    "demo.py",
+
+    "setup.py",
+
+
+    "config/model.yaml",
+
+    "config/schema.yaml",
+]
+
+
+###############################################################
+#------Boucle de création des fichiers et dossiers------#
+################################################################
+
+for filepath in list_of_files:
+    filepath = Path(filepath) #Détecte automatiquement le bon séparateur de dossier selon le système d'exploitation
+    filedir, filename = os.path.split(filepath) #Sépare le chemin du dossier et le nom du fichier
+
+    #Créer les dossiers si ils n'existent pas
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+
+    #Créer le fichier s'il n'existe pas ou s'il est vide
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+    else:
+        print(f"file is already present at: {filepath}")
